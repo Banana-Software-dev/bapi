96ee5d9389b0adbfe401f6feb3f0ca6667ff12eb
MrBisquit <william@wtdawson.info>
2023-08-02 12:47:17 +0100

    Updated some code.

--- lib/BAPI.ts
+++ lib/BAPI.ts
@@ -8,43 +8,47 @@
  * NPM - https://github.com/package/bapi
  */
 
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 import { invalidParameter } from "./exceptions";
 
 class BAPI_main {
-    private instance:object|undefined;
+    private instance:AxiosInstance|AxiosInstance;
     private API_key:string|undefined;
-    private options:object|undefined;
-    constructor (API_Key: string, Options: Options) {
+    private options:BAPI_Options|BAPI_Options;
+    constructor (API_Key: string, Options?: BAPI_Options) {
         this.API_key = API_Key;
-        this.options = Options;
+        this.options = Options ?? {
+            timeout: 1000
+        };
 
-        this.instance = axios.create({
-            baseURL: 'https://bananasoftware.dev/api/',
-            timeout: Options.timeout,
-            headers: {'api_token': this.API_key}
-        });
+        axios.defaults.baseURL = 'https://social.bananasoftware.dev/api/';
+        axios.defaults.timeout = this.options.timeout ?? 1000;
+        axios.defaults.headers.api_key = this.API_key;
+        axios.defaults.responseType = "json";
+    }
+
+    setOptions(Options?: BAPI_Options) {
+        this.options = Options ?? {
+            timeout: 1000
+        };
     }
 
     async fetchLatest(count: number) {
-        if (count > 5) new invalidParameter(count);
+        if (count > 5) return new invalidParameter(count);
         axios({
             method: 'get',
             url: '/posts/',
-            responseType: 'json',
-            headers: {"api_token": this.API_key}
-          })
+        })
         .then((response) => {
-            console.log(response)
+            
+        }).catch((err) => {
+
         });
     }
 }
 
-class Options {
-    public timeout:number|1000;
-    constructor (Timeout?: number) {
-        this.timeout = Timeout || 1000;
-    }
+type BAPI_Options = {
+    timeout: number
 }
 
-export { BAPI_main, Options };
+export { BAPI_main, BAPI_Options };
